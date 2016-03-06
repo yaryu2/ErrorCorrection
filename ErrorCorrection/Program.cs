d7f839da2c908c806dcb47ff421c80378cd33343
antiduh <antiduh@5b8169c6-5776-440f-8fd8-3ff31fe560f2>
2016-03-06 19:32:46 +0000

    Removed the old tests file; replaced the main call with a call to the full suite of tests.

--- ErrorCorrection/Program.cs
+++ ErrorCorrection/Program.cs
@@ -17,32 +17,8 @@ namespace ErrorCorrection
         [STAThread]
         static void Main()
         {
-            IntImplTester.StreamTest();
+            IntImplTester.DoTests();
             Console.Out.Flush();
-        }
-
-        private static void OldEncoderTest()
-        {
-            Rs256Decoder decoder = new Rs256Decoder( 16, 11, 0x13 );
-            {
-                byte[] origMessage = { 0, 0, 0, 0, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-                byte[] encodedMessage = { 12, 12, 3, 3, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-                byte[] errorMessage = { 12, 12, 1, 1, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-
-                decoder.Decode( errorMessage );
-                ArrayHelpers.CheckArrayEquals( errorMessage, encodedMessage );
-            }
-
-            Decoder oldDecoder = new Decoder( 16, 15, 11, 0x13 );
-            {
-                int[] origMessage = { 0, 0, 0, 0, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-                int[] encodedMessage = { 12, 12, 3, 3, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-                int[] errorMessage = { 12, 12, 1, 1, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-
-                oldDecoder.Decode( errorMessage );
-
-                ArrayHelpers.CheckArrayEquals( errorMessage, encodedMessage );
-            }
         }
     }
 }
